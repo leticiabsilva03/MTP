92b5ab448133dee275de95bcf8cc167c2e8196d9
leticiabsilva03 <leticiabsilva03@gmail.com>
2016-10-03 19:29:18 -0300

    fim da aula

--- aula20160906/ptr4.c
+++ aula20160906/ptr4.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
-int main () {
-    int num1, num2, soma;
-
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+    double n1,n2,soma;
+    printf("\n Digite dois numeros: \n");
+    scanf("%lf",&n1);
+    scanf("%lf",&n2);
+    soma = n1+n2;
+    printf("\n %p : %lf \n", &n1, n1);
+    printf("\n %p : %lf \n", &n2, n2);
+    printf("\n %p : %lf \n", &soma, soma);
     return 0;
 }
